d84de6e6cee7d04396a69354a9fa12354aed0920
Artem Korneev <artemkorneev@users.noreply.github.com>
2018-07-29 23:33:46 -0700

    Websocket - basic implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,29 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UnitService } from './services/unit.service';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
+import { WebsocketTestComponent } from './components/websocket-test/websocket-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     HomeComponent,
-    ViewRegistrationComponent
+    ViewRegistrationComponent,
+    WebsocketTestComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [UnitService],
   bootstrap: [AppComponent]
---
+++ src/app/components/websocket-test/websocket-test.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import '../../../sockjs.min.js';
+import '../../../stomp.min.js';
+
+@Component({
+  selector: 'app-websocket-test',
+  templateUrl: './websocket-test.component.html',
+  styleUrls: ['./websocket-test.component.css']
+})
+export class WebsocketTestComponent implements OnInit {
+
+  isConnected: boolean = false;
+  greetings: string = "";
+  stompClient: any;
+  it: any = this;
+  name: string = "no name specified yet";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  connect() {
+    var socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+    var stompClient = Stomp.over(socket);
+    this.isConnected = true;
+    stompClient.connect({}, this.onConnect.bind(this));
+    this.stompClient = stompClient;
+  }
+
+  onConnect(frame) {
+    this.stompClient.subscribe('/topic/greetings', this.onGreeting.bind(this));
+  }
+
+  onGreeting(greeting) {
+    this.showGreeting(JSON.parse(greeting.body).content);
+  }
+
+  sendName() {
+    if (this.name) {
+      this.stompClient.send("/app/hello", {}, JSON.stringify({ 'name': this.name }));
+    } else {
+      console.log("no name!");
+    }
+  }
+
+  disconnect() {
+    if (this.stompClient !== null) {
+        this.stompClient.disconnect();
+    }
+
+    this.isConnected = false;
+    console.log("Disconnected");
+  }
+
+  showGreeting(message) {
+    this.greetings += message + "\n\r";
+  }
+}
